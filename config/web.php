8487f2ee717948976c9033e337efa17780a59a26
hishnik <arenovec@gmail.com>
2021-04-04 16:32:42 +0300

    some edits

--- config/web.php
+++ config/web.php
@@ -1,86 +1,88 @@
 <?php
+$params = require(__DIR__ . '/params.php');
 
-    $params = require(__DIR__ . '/params.php');
-
-    $config = [
-        'id' => 'main',
-        'basePath' => dirname(__DIR__),
-        'bootstrap' => ['log'],
-        'modules' => [
-            'gridview' => [
-                'class' => '\kartik\grid\Module'
-            ],
+$config = [
+    'id' => 'main',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm' => '@vendor/npm-asset',
+    ],
+    'modules' => [
+        'gridview' => [
+            'class' => '\kartik\grid\Module'
         ],
-        'components' => [
-            'request' => [
-                // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-                'cookieValidationKey' => '12394567',
-                'baseURL' => '',
-                'class' => 'app\components\LangRequest',
-            ],
-            'authManager' => [
-                'class' => 'yii\rbac\DbManager',
-            //'cache' => 'cache' //Включаем кеширование 
-            ],
-            'cache' => [
-                'class' => 'yii\caching\FileCache',
-            ],
-            'user' => [
-                'identityClass' => 'app\models\Users',
-                'enableAutoLogin' => true,
-            ],
-            'errorHandler' => [
-                'errorAction' => 'site/error',
-            ],
-            'mailer' => [
-                'useFileTransport' => TRUE,
-            ],
-            'log' => [
-                'traceLevel' => YII_DEBUG ? 3 : 0,
-                'targets' => [
-                    [
-                        'class' => 'yii\log\FileTarget',
-                        'levels' => ['error', 'warning'],
-                    ],
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '12394567',
+            'baseURL' => '',
+            'class' => 'app\components\LangRequest',
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        //'cache' => 'cache' //Включаем кеширование 
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\Users',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'useFileTransport' => TRUE,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
                 ],
             ],
-            'db' => require(__DIR__ . '/db.php'),
-            'urlManager' => [
-                'enablePrettyUrl' => true,
-                'showScriptName' => false,
-                'class' => 'app\components\LangUrlManager',
-                'rules' => [],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'class' => 'app\components\LangUrlManager',
+            'rules' => [],
+        ],
+    ],
+    'controllerMap' => [
+        'elfinder' => [
+            'class' => 'mihaildev\elfinder\PathController',
+            'access' => ['@', '?'],
+            'root' => [
+                'path' => 'upload/page',
+                'name' => 'Page'
             ],
-        ],
-        'controllerMap' => [
-            'elfinder' => [
-                'class' => 'mihaildev\elfinder\PathController',
-                'access' => ['@', '?'],
-                'root' => [
-                    'path' => 'upload/page',
-                    'name' => 'Page'
-                ],
-            ]
-        ],
-        'params' => $params,
+        ]
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 
-    if(YII_ENV_DEV) {
-        // configuration adjustments for 'dev' environment
-        $config['bootstrap'][] = 'debug';
-        $config['modules']['debug'] = [
-            'class' => 'yii\debug\Module',
-            // uncomment the following to add your IP if you are not connecting from localhost.
-            'allowedIPs' => ['127.0.0.1', '::1'],
-        ];
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
 
-        $config['bootstrap'][] = 'gii';
-        $config['modules']['gii'] = [
-            'class' => 'yii\gii\Module',
-            // uncomment the following to add your IP if you are not connecting from localhost.
-            'allowedIPs' => ['127.0.0.1', '::1'],
-        ];
-    }
-
-    return $config;
-    
+return $config;
